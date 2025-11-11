929536bf905e4cb8b7c968caddb6e297de12e683
kamran-8674 <connect4kamran@gmail.com>
2025-11-11 22:10:30 +0600

    implemented hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const HeroSection = () => {
+    return (
+        <div>
+            <Swiper modules={[Navigation, Pagination, Autoplay]}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 4000 }}
+        loop={true}
+        className="h-[350px] w-full rounded-xl"
+            
+            >
+                <SwiperSlide>
+                     <img
+            src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg"
+            alt=""
+            className="w-full h-full mx-auto"
+          /> </SwiperSlide>
+                <SwiperSlide>
+                     <img
+            src="https://images.pexels.com/photos/1603901/pexels-photo-1603901.jpeg"
+            alt=""
+            className="w-full h-full mx-auto"
+          /> </SwiperSlide>
+                <SwiperSlide>
+            <img  
+            src="https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg"
+            alt=""
+            className="w-full h-full mx-auto"
+          /> </SwiperSlide>
+   </Swiper>
+            
+        </div>
+    );
+};
+
+export default HeroSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import FeaturedReview from '../components/FeaturedReview';
+import HeroSection from '../components/HeroSection';
 
 const Home = () => {
     return (
         <div>
-            
+            <HeroSection></HeroSection>
             <FeaturedReview></FeaturedReview>
+            
             
         </div>
     );
